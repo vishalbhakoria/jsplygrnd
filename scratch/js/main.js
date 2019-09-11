fb50655390aee1de8f6dc4d67b3222d9d19566e1
hikari89 <47591614+hikari89@users.noreply.github.com>
2019-09-11 09:07:43 +0800

    Add files via upload

--- scratch/js/main.js
+++ scratch/js/main.js
@@ -25,45 +25,56 @@ new ScrollMagic.Scene({
 .addTo(controller);
 
 var revealElements = document.getElementsByClassName("fade-in");
-for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+for (var i=0; i<revealElements.length; i++) { 
     new ScrollMagic.Scene({
-                        triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-                        offset: 100,												 // start a little later
+                        triggerElement: revealElements[i], 
+                        offset: 100,												 
                         triggerHook: 0.9,
                     })
-                    .setClassToggle(revealElements[i], "visible") // add class toggle
+                    .setClassToggle(revealElements[i], "visible") 
                     .addTo(controller);
 }
 
 revealElements = document.getElementsByClassName("fade-up");
-for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+for (var i=0; i<revealElements.length; i++) { 
     new ScrollMagic.Scene({
-                        triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-                        offset: 150,												 // start a little later
+                        triggerElement: revealElements[i], 
+                        offset: 150,												 
                         triggerHook: 0.9,
                     })
-                    .setClassToggle(revealElements[i], "visible") // add class toggle
+                    .setClassToggle(revealElements[i], "visible") 
                     .addTo(controller);
 }
 
 revealElements = document.getElementsByClassName("annual-value-left");
-for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+for (var i=0; i<revealElements.length; i++) { 
     new ScrollMagic.Scene({
-                        triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-                        offset: 100 + i*20,												 // start a little later
+                        triggerElement: revealElements[i], 
+                        offset: 100 + i*20,												 
                         triggerHook: 0.9,
                     })
-                    .setClassToggle(revealElements[i], "visible") // add class toggle
+                    .setClassToggle(revealElements[i], "visible") 
                     .addTo(controller);
 }
 
 revealElements = document.getElementsByClassName("callout");
-for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+for (var i=0; i<revealElements.length; i++) { 
     new ScrollMagic.Scene({
-                        triggerElement: '#comparable_img', // y value not modified, so we can use element as trigger as well
-                        offset: 100 + i*20,												 // start a little later
+                        triggerElement: '#comparable_img', 
+                        offset: 100 + i*20,												 
                         triggerHook: 0.9,
                     })
-                    .setClassToggle(revealElements[i], "visible") // add class toggle
+                    .setClassToggle(revealElements[i], "visible") 
+                    .addTo(controller);
+}
+
+revealElements = document.getElementsByClassName("fade-left");
+for (var i=0; i<revealElements.length; i++) { 
+    new ScrollMagic.Scene({
+                        triggerElement: revealElements[i], 
+                        offset: 100 + i*20,												 
+                        triggerHook: 0.9,
+                    })
+                    .setClassToggle(revealElements[i], "visible") 
                     .addTo(controller);
 }
