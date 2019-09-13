cc3901fd3a5016690c5d1fd66279f05cb4565422
hikari89 <47591614+hikari89@users.noreply.github.com>
2019-09-13 10:55:45 +0800

    Add files via upload

--- scratch/js/validation_page.js
+++ scratch/js/validation_page.js
@@ -2,3 +2,13 @@ function onSubmit()
 {
     window.location.replace('landing.html')
 }
+
+function validateRecaptcha(event)
+{
+    event.preventDefault();
+      grecaptcha.execute();
+}
+
+$(document).ready(function(){
+    $("#nric_submit_btn").click(validateRecaptcha);
+});
